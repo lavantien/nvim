e1aa2ec13dbf24e624b5b7fd747e708e53f3d1b7
lavantien <lavantien96@gmail.com>
2023-05-21 19:57:27 +0700

    Adjust remap

--- lua/lavantien/remap.lua
+++ lua/lavantien/remap.lua
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/lavantien/packer.lua<CR>");
 
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
+--vim.keymap.set("n", "<leader><leader>", function()
+--    vim.cmd("so")
+--end)
 
