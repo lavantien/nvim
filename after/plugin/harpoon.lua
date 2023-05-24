3a8d3d3e2a7db3f0ee76247b86637e6ad6d90a69
lavantien <lavantien96@gmail.com>
2023-05-24 21:06:20 +0700

    Optimize remappings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>vca", mark.add_file)
+vim.keymap.set("n", "<leader>va", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function()
