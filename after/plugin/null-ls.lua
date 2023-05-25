63d2fd552e7d9df9895ab94900659609844ef2e1
lavantien <lavantien96@gmail.com>
2023-05-25 16:20:10 +0700

    Fix conflict between lsp-zero and null-ls

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -5,56 +5,27 @@ local diagnostics = null_ls.builtins.diagnostics
 local actions = null_ls.builtins.code_actions
 
 local sources = {
-    formatting.gofumpt,
-    formatting.goimports_reviser,
-    formatting.dart_format,
-    formatting.rustfmt,
-    formatting.autopep8,
-    formatting.zigfmt,
-    --formatting.nimpretty,
-    formatting.xmlformat,
-    formatting.stylua,
-    formatting.yamlfmt,
-    formatting.shfmt,
-    formatting.sqlfmt,
-    formatting.nixfmt,
-    formatting.nginx_beautifier,
-    formatting.prettier,
-    formatting.clang_format,
-    formatting.markdownlint,
-    formatting.eslint,
+	formatting.dart_format,
+	formatting.zigfmt,
+	--formatting.nimpretty, -- not supported by the LS
+	formatting.nixfmt,
+	formatting.nginx_beautifier,
+	--formatting.clang_format, -- shadowing jdtls
 
-    diagnostics.golangci_lint,
-    diagnostics.protolint,
-    diagnostics.hadolint,
-    diagnostics.yamllint,
-    diagnostics.buf,
-    diagnostics.markdownlint,
-    diagnostics.terraform_validate,
-    diagnostics.ansiblelint,
-    diagnostics.actionlint,
-    diagnostics.dotenv_linter,
-    diagnostics.checkmake,
-    diagnostics.djlint,
-    diagnostics.eslint,
-    diagnostics.flake8,
-    diagnostics.clang_check,
-    diagnostics.codespell,
+	diagnostics.dotenv_linter,
+	diagnostics.checkmake,
+	diagnostics.clang_check,
 
-    actions.gomodifytags,
-    actions.impl,
-    actions.eslint,
-    actions.refactoring,
-    actions.shellcheck,
-    actions.gitsigns,
+	actions.refactoring,
+	actions.gitsigns,
 }
 
 --local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    sources = sources,
+	sources = sources,
 
-    --[==[
+	--[==[
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
--- lua/lavantien/packer.lua
+++ lua/lavantien/packer.lua
@@ -1,97 +1,97 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+    use("wbthomason/packer.nvim")
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{
-				-- Optional
-				"williamboman/mason.nvim",
-				run = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
-		},
-	})
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" }, -- Required
+            {
+                -- Optional
+                "williamboman/mason.nvim",
+                run = function()
+                    pcall(vim.cmd, "MasonUpdate")
+                end,
+            },
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },                  -- Required
+            { "hrsh7th/cmp-nvim-lsp" },              -- Required
+            { "L3MON4D3/LuaSnip" },                  -- Required
+        },
+    })
 
-	use({
-		"kylechui/nvim-surround",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	})
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end,
+    })
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.x",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.x",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-	use({
-		"rose-pine/neovim",
-		as = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end,
-	})
+    use({
+        "rose-pine/neovim",
+        as = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
+    })
 
-	use({
-		"folke/trouble.nvim",
-		requires = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({})
-		end,
-	})
+    use({
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup({})
+        end,
+    })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	})
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
+    })
 
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-	use("nvim-treesitter/playground")
-	use("jose-elias-alvarez/null-ls.nvim")
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use("nvim-treesitter/playground")
+    use("jose-elias-alvarez/null-ls.nvim")
 
-	use("theprimeagen/harpoon")
+    use("theprimeagen/harpoon")
 
-	use("theprimeagen/refactoring.nvim")
-	use("nvim-treesitter/nvim-treesitter-context")
+    use("theprimeagen/refactoring.nvim")
+    use("nvim-treesitter/nvim-treesitter-context")
 
-	use("mbbill/undotree")
+    use("mbbill/undotree")
 
-	use("tpope/vim-fugitive")
-	use("lewis6991/gitsigns.nvim")
+    use("tpope/vim-fugitive")
+    use("lewis6991/gitsigns.nvim")
 
-	use("j-hui/fidget.nvim")
+    use("j-hui/fidget.nvim")
 
-	use("Exafunction/codeium.vim")
-	use("wakatime/vim-wakatime")
+    use("Exafunction/codeium.vim")
+    use("wakatime/vim-wakatime")
 
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+    if packer_bootstrap then
+        require("packer").sync()
+    end
 end)
